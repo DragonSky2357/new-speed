bd384d8aecf820a211cd80f0d5551940d3391669
dragonsky2357 <qkrdydals327@gmail.com>
2024-02-08 19:07:04 +0900

    add : create security

---
+++ src/main/java/com/dragonsky/newspeed/security/JwtAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package com.dragonsky.newspeed.security;
+
+import com.dragonsky.newspeed.dto.member.LoginMemberDto;
+import com.dragonsky.newspeed.jwt.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@Slf4j(topic = "로그인 및 JWT 생성")
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+        setFilterProcessesUrl("/members/login"); // signin 요청이 들어올때 이곳으로 필터링 및 jwt 토큰 반환
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            LoginMemberDto requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginMemberDto.class);
+
+            // 사용자의 자격 증명을 검증하고, 사용자를 인증하는 과정
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getEmail(),
+                            requestDto.getPassword(),
+                            null
+                    )
+            );
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+        try {
+            String username = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
+            String token = jwtUtil.createToken(username);
+
+            response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("{\"statusCode\": 201, \"message\": \"성공적으로 인증되었습니다.\"}");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException {
+        try {
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("{\"statusCode\": 400, \"message\": \"회원을 찾을 수 없습니다.\"}");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/com/dragonsky/newspeed/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.dragonsky.newspeed.security;
+
+import com.dragonsky.newspeed.entity.Member;
+import com.dragonsky.newspeed.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final MemberRepository memberRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found Member"));
+
+        return new UserDetailsImpl(member);
+    }
+}
